34a4460893f0d1cdcd7a68eec7060af52ba8b0bf
Steven Kang <stevenkang@Stevens-MBP-2.wireless.yale.internal>
2016-01-22 23:27:56 -0500

    First commit

---
+++ GroupThink/GroupThink/VoteVC.h
@@ -0,0 +1,13 @@
+//
+//  VoteVC.h
+//  GroupThink
+//
+//  Created by Steven Kang on 1/22/16.
+//  Copyright © 2016 Steven Kang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VoteVC : UIViewController
+
+@end
